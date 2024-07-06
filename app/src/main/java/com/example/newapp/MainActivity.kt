ce432d5cbec7f6a88f970f9ca8648a6bb3272b9a
IvanPS1 <ivan-pankratov43@mail.ru>
2024-07-06 16:12:51 +0300

    Progress

--- app/src/main/java/com/example/newapp/MainActivity.kt
+++ app/src/main/java/com/example/newapp/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.newapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLearnWordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_learn_word)
+        binding = ActivityLearnWordBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+        }
 
     }
-}
